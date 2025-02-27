d786791df5f22ed84ea6f206d6b30a69ea878424
ARKASHARAB <barrbarianfonconrat@mail.ru>
2025-02-27 10:17:05 +0300

    Update header.js

--- js/header.js
+++ js/header.js
@@ -39,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
         { text: 'Мемы', href: '../pages/mem.html', imgSrc: '../img/s1.png' },
         { text: 'Пасты', href: '#', imgSrc: '../img/s1.png' },
         { text: 'Видево', href: '#', imgSrc: '../img/s1.png' },
+        { text: 'НЕГИФ', href: '#', imgSrc: '../img/s1.png' },
         { text: 'Войсы', href: '#', imgSrc: '../img/s1.png' },
         { text: 'Список', href: '#', imgSrc: '../img/s1.png' },
         { text: 'logo', href: '#', imgSrc: '../img/s1.png' }
